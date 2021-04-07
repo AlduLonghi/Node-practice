e2affc2ee5415d4a3524caba8a7928f1bfc3f254
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-04-07 12:45:02 -0300

    Setting up mongo as database

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { 
+    useNewUrlParser: true
+})
+
+const db = mongoose.connection
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Conected to mongoose'))
+
 app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000);
