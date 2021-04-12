784473edc576b8c63e6ef2656454f4fda5bbcf3c
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-04-12 12:54:29 -0300

    Update autghors route for using model

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Author = require('../models/auhtor');
 
 // All authors route
 router.get('/', (req, res) => {
@@ -8,7 +9,7 @@ router.get('/', (req, res) => {
 
 // New author route
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', {authors: new Author() })
 });
 
 // Create author route
