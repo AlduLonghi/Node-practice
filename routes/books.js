e25362328949cd8c55ed18083ae84f336586c34a
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-04-15 12:14:27 -0300

    Update route for new book

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models/book');
+const Author = require('../models/auhtor');
 
 // All books route
 router.get('/', async (req, res) => {
@@ -8,8 +9,17 @@ router.get('/', async (req, res) => {
 })
 
 // New book route
-router.get('/new', (req, res) => {
-   res.send('New book');
+router.get('/new', async (req, res) => {
+   try {
+     const authors = await Author.find({});
+     const book = new Book();
+     res.render('books/new', {
+         authors: authors,
+         book: book,
+     })
+   } catch {
+      res.redirect('/books');
+   }
 });
 
 // Create book route
